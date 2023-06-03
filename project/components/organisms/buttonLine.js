6db883116abb07af95d18ac8796fd39949780a47
vvnikita74 <124995385+vvnikita74@users.noreply.github.com>
2023-06-04 02:54:13 +0300

    button fix

--- project/components/organisms/buttonLine.js
+++ project/components/organisms/buttonLine.js
@@ -2,15 +2,18 @@ import styles from "../../styles/buttonLine.module.sass"
 import ActionButton from "../atoms/actionButton"
 import Like from "../../public/Like.svg"
 import X from "../../public/X.svg"
+import Star from "../../public/Star.svg"
+import Boost from "../../public/Boost.svg"
+import Back from "../../public/Back.svg"
 
 export default function ButtonLine () {
     return (
         <div className={styles.buttonLine}>
-            <ActionButton type={Like} active={false}/>
+            <ActionButton type={Back} active={false}/>
             <ActionButton type={X} active={true}/>
-            <ActionButton type={Like} active={false}/>
+            <ActionButton type={Star} active={false}/>
             <ActionButton type={Like} active={true}/>
-            <ActionButton type={X} active={false}/>
+            <ActionButton type={Boost} active={false}/>
         </div>
     )
 }
